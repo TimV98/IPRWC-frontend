d97e2d765ff64c1c07bc2f01caa0b9c1ea4c6310
Tim Vermaat <timvermaatxd@gmail.com>
2023-02-11 22:05:23 +0100

    fixed fethching the wrong user

--- security/admin-profile/admin-user-list/admin-user-item/admin-edit-user/admin-edit-user.component.ts
+++ security/admin-profile/admin-user-list/admin-user-item/admin-edit-user/admin-edit-user.component.ts
@@ -10,40 +10,34 @@ import {FormControl, FormGroup} from "@angular/forms";
   templateUrl: './admin-edit-user.component.html',
   styleUrls: ['./admin-edit-user.component.scss']
 })
-export class AdminEditUserComponent implements OnInit, OnDestroy {
+export class AdminEditUserComponent implements OnInit {
 
-  @Input() user: User = this.userService.user;
-
-  activeRoute: Subscription;
+  user: User = this.userService.user;
 
   userForm: FormGroup;
-
 
   constructor(private userService: UserService, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.activeRoute = this.route.params.subscribe(data =>
-      this.userService.getUser(data['id'])
-        .subscribe(data => {
-          this.userService.user = data
-          this.userForm = new FormGroup({
-            firstName: new FormControl(this.user.firstName),
-            prefix: new FormControl(this.user.prefix),
-            lastName: new FormControl(this.user.lastName),
-            street: new FormControl(this.user.street),
-            houseNumber: new FormControl(this.user.houseNumber),
-            zipCode: new FormControl(this.user.zipCode),
-            place: new FormControl(this.user.place),
-            phoneNumber: new FormControl(this.user.phoneNumber)
+    this.route.params.subscribe(data => {
+        this.userService.getUser(data['id'])
+          .subscribe(data => {
+            this.user = this.userService.user = data
+            this.userForm = new FormGroup({
+              firstName: new FormControl(this.user.firstName),
+              prefix: new FormControl(this.user.prefix),
+              lastName: new FormControl(this.user.lastName),
+              street: new FormControl(this.user.street),
+              houseNumber: new FormControl(this.user.houseNumber),
+              zipCode: new FormControl(this.user.zipCode),
+              place: new FormControl(this.user.place),
+              phoneNumber: new FormControl(this.user.phoneNumber)
+            })
           })
-        }))
-
+    })
   }
 
-  ngOnDestroy() {
-    this.activeRoute.unsubscribe()
-  }
 
   editUser() {
     this.user.firstName = this.userForm.value.firstName;
